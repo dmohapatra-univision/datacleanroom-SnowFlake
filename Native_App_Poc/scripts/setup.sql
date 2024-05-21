a7478eb2b5aee5d01ab2619dd2082455209a6631
dmohapatra <dmohapatra@univision.net>
2024-05-21 13:44:21 -0400

    updated

--- Native_App_Poc/scripts/setup.sql
+++ Native_App_Poc/scripts/setup.sql
@@ -1,1 +1,17 @@
 -- Setup script for the Hello Snowflake! application.
+
+CREATE APPLICATION ROLE app_native;
+CREATE SCHEMA IF NOT EXISTS native_app_poc;
+GRANT USAGE ON SCHEMA native_app_poc TO APPLICATION ROLE app_native;
+
+CREATE OR REPLACE PROCEDURE native_app_poc.HELLO()
+  RETURNS STRING
+  LANGUAGE SQL
+  EXECUTE AS OWNER
+  AS
+  BEGIN
+    RETURN 'Hello native_app_poc test';
+  END;
+
+
+GRANT USAGE ON PROCEDURE native_app_poc.HELLO() TO APPLICATION ROLE app_native;
