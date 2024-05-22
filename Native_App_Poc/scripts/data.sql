5c66d863196df678cc10a8fb9ae3f849746349d8
dmohapatra <dmohapatra@univision.net>
2024-05-22 13:00:16 -0400

    updated

--- Native_App_Poc/scripts/data.sql
+++ Native_App_Poc/scripts/data.sql
@@ -1,13 +1,13 @@
 CREATE SCHEMA IF NOT EXISTS shared_data;
-CREATE TABLE IF NOT EXISTS accounts (ID INT, NAME VARCHAR, VALUE VARCHAR);
-INSERT INTO accounts VALUES
+CREATE TABLE IF NOT EXISTS shared_data.accounts (ID INT, NAME VARCHAR, VALUE VARCHAR);
+INSERT INTO shared_data.accounts VALUES
   (1, 'Nihar', 'Snowflake'),
   (2, 'Frank', 'Snowflake'),
   (3, 'Benoit', 'Snowflake'),
   (4, 'Steven', 'Acme');
 
 GRANT USAGE ON SCHEMA shared_data TO SHARE IN APPLICATION PACKAGE native_app_poc;
-GRANT SELECT ON TABLE accounts TO SHARE IN APPLICATION PACKAGE native_app_poc;  
+GRANT SELECT ON TABLE shared_data.accounts TO SHARE IN APPLICATION PACKAGE native_app_poc;  
 
 CREATE OR ALTER VERSIONED SCHEMA code_schema;
 GRANT USAGE ON SCHEMA code_schema TO APPLICATION ROLE app_native;
