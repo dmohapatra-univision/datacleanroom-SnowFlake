c3782004768e29677e56ce726af340b437d8f03b
dmohapatra <dmohapatra@univision.net>
2024-05-22 13:15:24 -0400

    updated

--- Native_App_Poc/scripts/data.sql
+++ Native_App_Poc/scripts/data.sql
@@ -1,18 +1,19 @@
-CREATE SCHEMA IF NOT EXISTS shared_data;
-CREATE TABLE IF NOT EXISTS shared_data.accounts (ID INT, NAME VARCHAR, VALUE VARCHAR);
-INSERT INTO shared_data.accounts VALUES
+
+CREATE SCHEMA IF NOT EXISTS native_app_poc.shared_data;
+CREATE TABLE IF NOT EXISTS native_app_poc.shared_data.accounts (ID INT, NAME VARCHAR, VALUE VARCHAR);
+INSERT INTO native_app_poc.shared_data.accounts VALUES
   (1, 'Nihar', 'Snowflake'),
   (2, 'Frank', 'Snowflake'),
   (3, 'Benoit', 'Snowflake'),
   (4, 'Steven', 'Acme');
 
-GRANT USAGE ON SCHEMA shared_data TO SHARE IN APPLICATION PACKAGE native_app_poc;
-GRANT SELECT ON TABLE shared_data.accounts TO SHARE IN APPLICATION PACKAGE native_app_poc;  
+GRANT USAGE ON SCHEMA native_app_poc.shared_data TO SHARE IN APPLICATION PACKAGE native_app_poc;
+GRANT SELECT ON TABLE native_app_poc.shared_data.accounts TO SHARE IN APPLICATION PACKAGE native_app_poc;  
 
-CREATE OR ALTER VERSIONED SCHEMA code_schema;
-GRANT USAGE ON SCHEMA code_schema TO APPLICATION ROLE app_native;
+CREATE OR ALTER VERSIONED SCHEMA native_app_poc.code_schema;
+GRANT USAGE ON SCHEMA native_app_poc.code_schema TO APPLICATION ROLE app_native;
 
-CREATE VIEW IF NOT EXISTS code_schema.accounts_view
+CREATE VIEW IF NOT EXISTS native_app_poc.code_schema.accounts_view
   AS SELECT ID, NAME, VALUE
   FROM shared_data.accounts;
-GRANT SELECT ON VIEW code_schema.accounts_view TO APPLICATION ROLE app_native;
+GRANT SELECT ON VIEW native_app_poc.code_schema.accounts_view TO APPLICATION ROLE app_native;
