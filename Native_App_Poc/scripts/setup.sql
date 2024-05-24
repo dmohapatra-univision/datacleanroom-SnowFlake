487852b1feb0e56924a100db14a444e326949465
dmohapatra <dmohapatra@univision.net>
2024-05-24 10:03:55 -0400

    updated

--- Native_App_Poc/scripts/setup.sql
+++ Native_App_Poc/scripts/setup.sql
@@ -16,13 +16,13 @@ CREATE OR REPLACE PROCEDURE native_app_poc.HELLO()
 
 GRANT USAGE ON PROCEDURE native_app_poc.HELLO() TO APPLICATION ROLE app_native;
 
-CREATE or REPLACE FUNCTION native_app_poc.multiply(num1 float, num2 float)
+EXECUTE IMMEDIATE FROM './data.sql';
+
+CREATE or REPLACE FUNCTION code_schema.multiply(num1 float, num2 float)
   RETURNS float
   LANGUAGE PYTHON
   RUNTIME_VERSION=3.8
   IMPORTS = ('/python/hello_python.py')
   HANDLER='hello_python.multiply';
 
-GRANT USAGE ON FUNCTION native_app_poc.multiply(FLOAT, FLOAT) TO APPLICATION ROLE app_native;
-
-EXECUTE IMMEDIATE FROM './data.sql';    
+GRANT USAGE ON FUNCTION code_schema.multiply(FLOAT, FLOAT) TO APPLICATION ROLE app_native;
